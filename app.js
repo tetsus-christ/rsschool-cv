52cc5440c6aaf1c446122636ba0d486d246e7e9d
tetsus-christ <tetsus.christ@gmail.com>
2024-07-04 22:16:41 +0300

    feat: add close tabs

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 function openTab(ref) {
     var tabs = document.getElementsByClassName("tab");
-    var mainPadding = document.getElementsByClassName("main");
     for (let i = 0; i < tabs.length; i++) {
       tabs[i].style.display = "none";
       document.getElementById("main").style.padding = "0";
     }
     document.getElementById(ref).style.display = "block";
-    document.getElementById("main").style.padding = "10px";
+    document.getElementById("main").style.padding = "20px";
+  }
+
+  function closeTabs() {
+    var tabs = document.getElementsByClassName("tab");
+    for (let i = 0; i < tabs.length; i++) {
+      tabs[i].style.display = "none";
+      document.getElementById("main").style.padding = "0";
+    }
   }
