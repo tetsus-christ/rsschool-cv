0bf78e3a0638927adf5f8b648d4fdc13f95542c1
tetsus-christ <tetsus.christ@gmail.com>
2024-07-04 20:11:44 +0300

    feat: all tabs closed at start

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 function openTab(ref) {
     var tabs = document.getElementsByClassName("tab");
+    var mainPadding = document.getElementsByClassName("main");
     for (let i = 0; i < tabs.length; i++) {
       tabs[i].style.display = "none";
+      document.getElementById("main").style.padding = "0";
     }
     document.getElementById(ref).style.display = "block";
+    document.getElementById("main").style.padding = "10px";
   }
