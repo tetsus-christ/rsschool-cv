4d2032fe6837a6a2e7aa4b7885635a9aa6814ec0
tetsus-christ <tetsus.christ@gmail.com>
2024-07-05 13:41:43 +0300

    feat: style buttons

--- app.js
+++ app.js
@@ -1,17 +1,47 @@
-function openTab(ref) {
+function openTab(ref, button) {
+    var i;
+    var tabs = document.getElementsByClassName("tab");
+    for (i = 0; i < tabs.length; i++) {
+      tabs[i].style.display = "none";
+      document.getElementById("main").style.padding = "0";
+    }
+
+    var buttons = document.getElementsByClassName("button");
+    for (i = 0; i < buttons.length; i++) {
+      buttons[i].style.backgroundColor = ""
+      buttons[i].style.color = "";
+    }
+
+    document.getElementById(ref).style.display = "block";
+    button.style.backgroundColor = "#36283B";
+    button.style.color = "#cf548b";
+    document.getElementById("main").style.padding = "20px";
+  }
+
+
+
+  function closeTabs() {
+
     var tabs = document.getElementsByClassName("tab");
     for (let i = 0; i < tabs.length; i++) {
       tabs[i].style.display = "none";
       document.getElementById("main").style.padding = "0";
     }
-    document.getElementById(ref).style.display = "block";
-    document.getElementById("main").style.padding = "20px";
+
+    var buttons = document.getElementsByClassName("button");
+    for (i = 0; i < buttons.length; i++) {
+      buttons[i].style.backgroundColor = ""
+      buttons[i].style.color = "";
+    }
+
   }
 
-  function closeTabs() {
-    var tabs = document.getElementsByClassName("tab");
-    for (let i = 0; i < tabs.length; i++) {
-      tabs[i].style.display = "none";
-      document.getElementById("main").style.padding = "0";
+
+  function myFunction() {
+    var nav = document.getElementById("navigation");
+    if (window.screen.width < 800) {
+      nav.style.display = "none";
+    } else {
+      nav.style.display = "block";
     }
   }
