5c9b73f0d226368470d0aa0debd80c86bbb718fe
tetsus-christ <tetsus.christ@gmail.com>
2024-07-05 22:26:20 +0300

    refactor: minor design changes

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 function openTab(ref, button) {
     var i;
-    var tabs = document.getElementsByClassName("tab");
+    var tabs = document.getElementsByClassName('tab');
     for (i = 0; i < tabs.length; i++) {
       tabs[i].style.display = "none";
     }
 
-    var buttons = document.getElementsByClassName("button");
+    var buttons = document.getElementsByClassName('button');
     for (i = 0; i < buttons.length; i++) {
-      buttons[i].style.backgroundColor = ""
-      buttons[i].style.color = "";
+      buttons[i].style.backgroundColor = ''
+      buttons[i].style.color = '';
     }
 
-    document.getElementById(ref).style.display = "block";
-    button.style.backgroundColor = "#36283B";
-    button.style.color = "#cf548b";
+    document.getElementById(ref).style.display = 'block';
+    button.style.backgroundColor = '#36283B';
+    button.style.color = '#cf548b';
   }
 
-  document.getElementById("default-tab").click();
+  document.getElementById('default-tab').click();
